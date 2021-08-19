82dc3f32c225f3f22ea639376426393c893c4b7f
Yathusanan <imyathusanan@gmail.com>
2021-08-19 21:28:28 +0530

    Initial Commit

---
+++ GreenFarm.API/Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,19 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Prodcuts { get; set; }
+    }
+}
